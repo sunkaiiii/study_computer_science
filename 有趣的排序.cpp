275efaf04bff5bdf487153d3e3c635bd35f5ece4
孙楷 <704725181@qq.com>
2017-08-19 22:34:19 +0800

    8.19

--- 有趣的排序.cpp
+++ 有趣的排序.cpp
@@ -35,16 +35,21 @@ int main()
 		b[i]=a[i];
 	}
 	std::sort(a,a+n);
-	int result=0;
+	int j=0;
+	int count=0;
 	for(int i=0;i<n;i++)
 	{
-		for(int j=0;j<n;j++)
+		if(b[i]==a[j])
 		{
-			if(b[j]==a[i])
-			{
-				result+=fabs(j-i);
-			}
+			count++;
+			j++;
 		}
 	}
-	std::cout<<result/2<<std::endl;
+	std::cout<<n-count<<std::endl;
 }
+/*复杂度 nlogn 但是需要一个 n 的辅助数组
+*用一个辅助数组对所有元素进行排序，从小到大排序
+*从第一个排好序的元素开始，即最小的元素开始与没排好序数组元素比较，
+*检查有多少个已经是从最小到大好序的，位置可以不连续，但是大的元素必须在小的元素后面，
+* 统计出一共有 count个，这些元素是不需要移动的元素
+* 一共有 n 个元素，所以需要移动 n - count 次
