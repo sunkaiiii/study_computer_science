02ee28b552d71928f80993348402a70499e936eb
孙楷 <704725181@qq.com>
2017-09-04 23:18:42 +0800

    9.4

---
+++ Graph/Graph.java
@@ -0,0 +1,95 @@
+package Graph;
+
+import StackAndQueue.Bag;
+
+import java.util.Scanner;
+
+/**
+ * Created by sunkai on 2017/9/4.
+ */
+public class Graph {
+    private final int V; //定点数目
+    private int E;  //边的数目
+    private Bag<Integer>[] adj; //邻接表
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public Graph(Scanner in) {
+        this(in.nextInt());
+        int E = in.nextInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.nextInt();
+            int w = in.nextInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    @Override
+    public String toString() {
+        String s = V + "vertices, " + E + " edges\n";
+        for (int v = 0; v < V; v++) {
+            s += v + ": ";
+            for (int w : this.adj(v)) {
+                s += w + " ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adj(v))
+            degree++;
+        return degree;
+    }
+
+    public static int maxDegree(Graph G) {
+        int max = 0;
+        for (int v = 0; v < G.V(); v++) {
+            if (degree(G, v) > max)
+                max = degree(G, v);
+        }
+        return max;
+    }
+
+    public static double avgDegree(Graph G) {
+        return 2.0 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G) {
+        int count = 0;
+        for (int v = 0; v < G.V(); v++) {
+            for (int w : G.adj(v)) {
+                if (v == w)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
