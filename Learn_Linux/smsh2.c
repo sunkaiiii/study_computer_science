5fe14c535835237e6731da5424478f4fca37ee20
sunkaiiii <704725181@qq.com>
2017-10-07 15:29:46 +0800

    10.7

---
+++ Learn_Linux/smsh2.c
@@ -0,0 +1,42 @@
+/*
+一个小型的shell，输入一个字符串，然后使用fork exec wait 然后屏蔽信号量
+第二版添加了对于if...then的支持
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+#include "smsh.h"
+#define DFL_PROMPT ">"
+
+int main()
+{
+  char *cmdline,*prompt,**arglist;
+  int result,process(char**);
+  void setup();
+
+  prompt=DFL_PROMPT;
+  setup();
+
+  while((cmdline=next_cmd(prompt,stdin))!=NULL)
+  {
+    if((arglist=splitline(cmdline))!=NULL)
+    {
+      result=process(arglist);
+      freelist(arglist);
+    }
+    free(cmdline);
+  }
+  return 0;
+}
+void setup()
+{
+  signal(SIGINT,SIG_IGN);
+  signal(SIGQUIT,SIG_IGN);
+}
+
+void fatal(char *s1,char *s2,int n)
+{
+  fprintf(stderr,"Error:%s,%s\n",s1,s2);
+  exit(n);
+}
