1e4a21ad25e8a94d8c37b0a6845ff092c1e82ce6
孙楷 <704725181@qq.com>
2017-10-22 13:17:37 +0800

    10.22

--- Learn_Cpp/有关模板的一些.cpp
+++ Learn_Cpp/有关模板的一些.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<memory>
 #include<functional>
+#include<utility>
 template <typename T> class Blob
 {
 public:
@@ -49,6 +50,39 @@ public:
 	}
 };
 
+
+template<typename T> T fcn(T val)
+{
+	val=3;
+	return val;
+}
+//当用右值调用的时候，局部变量t是int，当对t进行赋值的时候，val保持不变
+//当用左值调用的时候，t是&int，因此t初始化绑定了val，对t的修改也会体现在val上，val==t也为true
+template<typename T> void f3(T&& val)
+{
+	T t=val;
+	t=fcn(t);
+	if(val==t)
+		std::cout<<"true\n";
+	else
+		std::cout<<"false\n";
+}
+
+template<typename F,typename T1,typename T2>
+void flip(F f,T1 &&t1,T2 &&t2)
+{
+	//forward函数它能够保持原始实参的类型，类似std::move
+	//forward返回改显示实参的右值引用
+	//如果实参是一个普通类型，则返回Type&&
+	//如果实参是一个左值，则通过引用折叠，返回一个指向左值引用的右值引用
+	f(std::forward<T2>(t2),std::forward<T1>(t1));
+}
+
+void f(int v1,int &v2)
+{
+	std::cout<<v1<<" "<<++v2<<std::endl;
+}
+
 int main()
 {
 	std::cout<<compare(2,1)<<std::endl; //模板默认实参的测试
@@ -58,4 +92,16 @@ int main()
 	DebugDelete debug_delete(std::cout); //成员模板的一个使用测试
 	debug_delete(a);
 	debug_delete(a2);
+	int val=4;
+	f3(val); //val的值被改变
+	int &&a3=2; //a3的值被改变
+	f3(a3);
+	f3(42); //42的值不会被改变
+	std::cout<<val<<"\n";
+	std::cout<<a3<<"\n";
+	int v1=3;
+	int v2=4;
+	std::cout<<"v1,v2: "<<v1<<" "<<v2<<"\n";
+	flip(f,v1,v2);
+	std::cout<<"v1,v2: "<<v1<<" "<<v2<<"\n";
 }
