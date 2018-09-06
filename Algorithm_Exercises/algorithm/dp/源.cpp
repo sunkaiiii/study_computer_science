2fd6705afe4789b1fa1e9ed2e2aada7f5b4ae868
kai2.sun <kai2.sun@ly.com>
2018-09-06 17:49:35 +0800

    9.6
    Co-Authored-By: 孙楷 <704725181@qq.com>

--- Algorithm_Exercises/algorithm/dp/源.cpp
+++ Algorithm_Exercises/algorithm/dp/源.cpp
@@ -105,9 +105,12 @@ void LCS(const std::string s1, const std::string s2) {
 	//最终时间复杂度O(N方)
 }
 
+
+//数据压缩
 void traceBack(int n, std::vector<int> l);
 int length(int i);
 void compress(std::vector<int> origin) {
+	//header存储两个信息 前8个存储数量，后3位每一段的存储位数，则一共需要11位长
 	const static int maxLength = 256;
 	const static int header = 11;
 	std::vector<int> s;
@@ -121,15 +124,15 @@ void compress(std::vector<int> origin) {
 		p.push_back(value);
 	}
 	for (int i = 1; i < p.size(); i++){
-		b.push_back(length(p[i]));
-		int bmax = b[i];
-		s.push_back(s[i - 1] + bmax);
+		b.push_back(length(p[i])); //表示当前数字的位数
+		int bmax = b[i]; 
+		s.push_back(s[i - 1] + bmax); //这一次循环的数字单成一段
 		l.push_back(1);
-		for (int j = 2; j <= std::min(i, maxLength);j++) {
-			if (bmax < b[i - j + 1]) {
+		for (int j = 2; j <= std::min(i, maxLength);j++) { //开始判断与前面的数字共成一段的效果
+			if (bmax < b[i - j + 1]) { //统一这一段数字的位数，如果当前位数小，则扩大到统一大小
 				bmax = b[i - j + 1];
 			}
-			if (s[i] > s[i - j] + j * bmax) {
+			if (s[i] > s[i - j] + j * bmax) { //开始判断这一段是否为当前的最优解
 				s[i] = s[i - j] + j * bmax;
 				l[i] = j;
 			}
@@ -145,12 +148,8 @@ void compress(std::vector<int> origin) {
 }
 
 int length(int i) {
-	int count = 0;
-	while (i > 0) {
-		i = i >> 1;
-		count++;
-	}
-	return count;
+	int ii = ceil(log10(i+1)/log10(2));
+	return ii;
 }
 
 void traceBack(int n, std::vector<int> l) {
@@ -171,6 +170,6 @@ int main() {
 	//std::string s1, s2;
 	//std::cin >> s1 >> s2;
 	//LCS(s1, s2);
-	std::vector<int> p{ 10,12,15,255,1,2};
+	std::vector<int> p{ 10,12,15,255,1,2,1,1,2,2,1,1};
 	compress(p);
 }
