319b87f4a4945928fc8205915ac3829bbb57f093
孙楷 <704725181@qq.com>
2019-07-17 13:58:13 +1000

    删除Java并发相关内容
    那本书太烂了。
    另外再看一本吧

--- Learn_Java/LearnJava/src/No3_Concurrency_Programming_with_Java_NOT_RECOMMEND/cp3/model/Sample.java
+++
@@ -1,6 +0,0 @@
-package No3_Concurrency_Programming_with_Java_NOT_RECOMMEND.cp3.model;
-
-public abstract class Sample {
-	public abstract String getTag();
-	public abstract double[] getExample();
-}
--- Learn_Java/LearnJava/src/No3_Concurrency_Programming_with_Java_NOT_RECOMMEND/cp7/search/Main.java
+++
@@ -1,4 +0,0 @@
-package No3_Concurrency_Programming_with_Java_NOT_RECOMMEND.cp7.search;
-
-public class Main {
-}
