c32468e616d31c94e9b578c78e8cc3a4269abdf2
孙楷 <704725181@qq.com>
2019-10-12 13:59:55 +1100

    深入理解C# 第16章
    第16章：尾声

---
+++ Learn_CSharp/CP16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CP16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# 5当中得到修改的foreach循环中变量捕获的问题
+            string[] values = { "x", "y", "z" };
+            var actions = new List<Action>();
+
+            foreach(string value in values)
+            {
+                actions.Add(() => Console.WriteLine(value));
+            }
+
+            foreach(var action in actions)
+            {
+                action(); //会正确的显示x,y,z
+            }
+        }
+    }
+}
