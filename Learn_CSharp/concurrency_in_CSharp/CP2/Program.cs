bd3c4bf738b17c60db7e101832104f66dab46d24
孙楷 <704725181@qq.com>
2019-11-24 18:09:27 +1100

    C#并发编程实例 第一张，第二章

---
+++ Learn_CSharp/concurrency_in_CSharp/CP2/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CP2
+{
+    class Program
+    {
+        //指数避退
+        static async Task<string> DownloadStringWithRetries(string url)
+        {
+            using(var client=new HttpClient())
+            {
+                var nextDelay = TimeSpan.FromSeconds(1);
+                for(int i=0;i!=3;i++)
+                {
+                    try
+                    {
+                        return await client.GetStringAsync(url);
+                    }
+                    catch { }
+                    await Task.Delay(nextDelay);
+                    nextDelay = nextDelay + nextDelay;
+                }
+                return await client.GetStringAsync(url); //最后重试，并不捕获异常
+
+            }
+        }
+
+        //Task Delay所提供的超时功能
+        static async Task<String> DownloadSttringWithTimeout(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var downloadTask = client.GetStringAsync(url);
+                var timeoutTask = Task.Delay(3000);
+                var completedTask = await Task.WhenAny(downloadTask, timeoutTask);
+                if (completedTask == timeoutTask)
+                    return null;
+                return await downloadTask;
+            }
+        }
+
+        //返回完成的任务
+        //具有异步签名的同步方法
+        interface IMyAsyncInterface
+        {
+            Task<int> GetValueAsync();
+        }
+        class MySynchronousImplementation:IMyAsyncInterface
+        {
+            public Task<int> GetValueAsync()
+            {
+                return Task.FromResult(13);
+            }
+        }
+
+        //报告进度
+        static async Task MyMethodAsync(IProgress<int> progress=null)
+        {
+            int percentComplete = 0;
+            for(;percentComplete<100;percentComplete++)
+            {
+                if(progress!=null)
+                {
+                    progress.Report(percentComplete);
+                }
+            }
+        }
+        static async Task CallMyMethodAsync()
+        {
+            var progess = new Progress<int>();
+            progess.ProgressChanged += (sender, args) =>
+              {
+                  Console.WriteLine(args);
+              };
+            await MyMethodAsync(progess);
+        }
+
+        //等待一组任务完成
+        static async Task<int[]> WhenAllAsync()
+        {
+            var task1 = Task.FromResult(3);
+            var task2 = Task.FromResult(5);
+            var task3 = Task.FromResult(7);
+            return await Task.WhenAll(task1, task2, task3);
+        }
+
+        //使用WhenAll进行下载
+        static async Task<string> DownloadAllAsync(IEnumerable<string> urls)
+        {
+            var httpClient = new HttpClient();
+            //定义每一个url的使用方法
+            var downloads = urls.Select(url => httpClient.GetStringAsync(url));
+            //同步开始所有的下载
+            Task<string>[] downloadTasks = downloads.ToArray();
+            //所有的下载就已经开始了
+            string[] htmlPages = await Task.WhenAll(downloadTasks);
+            return string.Concat(htmlPages);
+        }
+
+        //使用WhenAny判断第一个相应的URL的数据长度
+        static async Task<int> FirstRespondingUrlAsync(IEnumerable<string> urls)
+        {
+            var httpClient = new HttpClient();
+            var downloads = urls.Select(url => httpClient.GetByteArrayAsync(url));
+            //同步开始所有的下载
+            Task<byte[]>[] downloadTasks = downloads.ToArray();
+            //等待任意一个任务完成
+            Task<byte[]> completedTask = await Task.WhenAny(downloads);
+            byte[] data = await completedTask;
+            return data.Length;
+        }
+
+        //任务完成时的处理
+        static async Task<int> DelayAndReturnAsync(int val)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(val));
+            return val;
+        }
+
+        static async Task AwaitAndProcessAsync(Task<int> task)
+        {
+            var result = await task;
+            Console.WriteLine(result);
+        }
+
+        //现在，方法输出1，2，3
+        static async Task ProcessTaskAsync()
+        {
+            var taskA = DelayAndReturnAsync(2);
+            var taskB = DelayAndReturnAsync(3);
+            var taskC = DelayAndReturnAsync(1);
+            var tasks = new[] { taskA, taskB, taskC };
+            var processingTasks = (from t in tasks select AwaitAndProcessAsync(t)).ToArray();
+            await Task.WhenAll(processingTasks);
+        }
+
+        static void Main(string[] args)
+        {
+            var urls = new string[]
+            {
+                "https://sunkai.xyz:5001/api/NewsList/1",
+                "https://sunkai.xyz:5001/api/NewsList/2",
+                "https://sunkai.xyz:5001/api/NewsList/3",
+                "https://sunkai.xyz:5001/api/NewsList/4",
+                "https://sunkai.xyz:5001/api/NewsList/5"
+            };
+            Console.WriteLine(DownloadAllAsync(urls.ToList()).Result);
+
+            Console.WriteLine(FirstRespondingUrlAsync(urls).Result);
+
+            ProcessTaskAsync().Wait();
+        }
+    }
+}
---
+++ Learn_CSharp/concurrency_in_CSharp/concurrency_in_CSharp/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace concurrency_in_CSharp
+{
+    class Program
+    {
+        async Task DoSomeThingAsync()
+        {
+            int val = 13;
+            //异步方式等待1秒
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            val *= 2;
+            //异步方式等待一秒
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            Trace.WriteLine(val.ToString());
+        }
+
+        async Task WaitAsync()
+        {
+            //这里await会捕获当前上下文
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            //这里会试图用上面捕获的上下文继续执行。
+        }
+
+        void DeadLock()
+        {
+            Task task = WaitAsync();
+            //执行同步程序块，正在等待异步方法完成
+            task.Wait();
+            //如果UI或者ASP.NET的上下文调用这个代码，就会发生死锁
+            //因为两种上下文每次只能运行一个线程。
+            WaitAsync().ConfigureAwait(false); //解决方法
+        }
+
+        //一种实现数据并行的方法
+        void CountNumbers(IEnumerable<int> numbers)
+        {
+            Parallel.ForEach(numbers, number => number.ToString());
+        }
+
+        IEnumerable<bool> PrimalityTest(IEnumerable<int> values)
+        {
+            return values.AsParallel().Select(val => val % 2 == 0);
+        }
+
+        //Parallel类的Fork/Join来实现任务并行
+        void ProcessArray(double[] array)
+        {
+            Parallel.Invoke(() => ProcessPartialArray(array, 0, array.Length / 2),
+                () => ProcessPartialArray(array, array.Length / 2, array.Length));
+        }
+
+        private void ProcessPartialArray(double[] array, int v1, int v2)
+        {
+            //
+        }
+
+        void ExceptionHandle()
+        {
+            //由于操作是并行执行的，多个一长就会同时发生
+            //系统会把这些异常封装在AggregateException里.
+            try
+            {
+                Parallel.Invoke(() => throw new Exception(),
+                    () => throw new Exception());
+            }catch(AggregateException ex)
+            {
+                ex.Handle(exception =>
+                {
+                    Trace.WriteLine(exception);
+                    return true; //已经处理
+                });
+            }
+        }
+
+        void TPL()
+        {
+            try
+            {
+                var multiplyBlock = new TransformBlock<int, int>(item =>
+                   {
+                       if (item == 1)
+                           throw new InvalidOperationException("Blech.");
+                       return item * 2;
+                   });
+                var subtractBlock = new TransformBlock<int, int>(item => item - 2);
+                multiplyBlock.LinkTo(subtractBlock, new DataflowLinkOptions { PropagateCompletion = true });
+                multiplyBlock.Post(1);
+                subtractBlock.Completion.Wait();
+            }catch(AggregateException exception)
+            {
+                AggregateException ex = exception.Flatten();
+                Trace.WriteLine(ex.InnerException);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var task = new Program().DoSomeThingAsync();
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+        }
+    }
+}
