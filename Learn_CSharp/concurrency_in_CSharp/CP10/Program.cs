6ed8ebabb9b748e54c7ed6f4e72e14633049dcee
孙楷 <704725181@qq.com>
2019-12-04 19:03:55 +0800

    C#并发编程实例第10章，第11章
    第10章：函数式OOP
    第11章：同步

---
+++ Learn_CSharp/concurrency_in_CSharp/CP10/Program.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.VisualStudio.Threading;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CP10
+{
+    class Program
+    {
+        //下面的代码定义了一个包含异步方法（不用async）的接口，对该接口的实现（用async），还定义了一个独立的方法，来调用该接口的方法
+        interface IMyAsyncInterface
+        {
+            Task<int> CountBytesAsync(string url);
+        }
+
+        class MyAsyncClass : IMyAsyncInterface
+        {
+            public async Task<int> CountBytesAsync(string url)
+            {
+                var client = new HttpClient();
+                var bytes = await client.GetByteArrayAsync(url);
+                return bytes.Length;
+            }
+        }
+        static async Task UseMyInterfaceAsync(IMyAsyncInterface service)
+        {
+            var result = await service.CountBytesAsync("https://www.baidu.com");
+            Console.WriteLine(result);
+        }
+
+        //下面这段代码展示了异步工厂代码的方法模式
+        class MyAsyncClassFactory
+        {
+            private MyAsyncClassFactory() { }
+
+            private async Task<MyAsyncClassFactory> InitialiseAsync()
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                return this;
+            }
+
+            public static Task<MyAsyncClassFactory> createAsync()
+            {
+                var result = new MyAsyncClassFactory();
+                return result.InitialiseAsync();
+            }
+        }
+
+        //异步初始化，非使用工厂模式
+        public interface IAsyncInitialisation
+        {
+            Task Initialisation { get; }
+        }
+        class MyFundamentalType: IAsyncInitialisation
+        {
+            public MyFundamentalType()
+            {
+                Initialisation = InitialiseAsync();
+            }
+            public Task Initialisation { get; private set; }
+
+            private async  Task InitialiseAsync()
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        //使用异步的Lazy对象实现异步属性
+        class AsyncProperty
+        {
+            public AsyncLazy<int> Data
+            {
+                get { return _data; }
+            }
+            private readonly AsyncLazy<int> _data = new AsyncLazy<int>(async () =>
+              {
+                  await Task.Delay(TimeSpan.FromSeconds(1));
+                  return 13;
+              });
+        }
+
+        //取消异步
+        class MyClass:IDisposable
+        {
+            private readonly CancellationTokenSource _disposeCts = new CancellationTokenSource();
+
+            public async Task<int> CalculateValueAsync()
+            {
+                //使用dispose的基本模式
+                await Task.Delay(TimeSpan.FromSeconds(2), _disposeCts.Token);
+                return 13;
+            }
+            public async Task<int> CalculateValueAsync(CancellationToken cancellation)
+            {
+                //实际应该在多一些检查
+                //检查对象有没有被销毁
+                using (var combinedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellation, _disposeCts.Token))
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(2), combinedCts.Token);
+                    return 13;
+                }
+
+            }
+            public void Dispose() //当取消的时候，会执行Dispose方法，随即执行Cancel
+            {
+                _disposeCts.Cancel();
+            }
+
+            async Task Test()
+            {
+                Task<int> task;
+                using (var resourse = new MyClass())
+                {
+                    task = resourse.CalculateValueAsync();
+                }
+                //抛出异常
+                var result = await task;
+            }
+        }
+
+        //异步完成取消
+        interface IAsyncCompletion
+        {
+            void Complete();
+            Task Completion { get; }
+        }
+        class MyClassCompletion:IAsyncCompletion
+        {
+            private readonly TaskCompletionSource<object> _completion = new TaskCompletionSource<object>();
+            private Task _completing;
+            public Task Completion
+            {
+                get { return _completion.Task; }
+            }
+
+            public void Complete()
+            {
+                if (_completing != null)
+                    return;
+                _completing = CompleteAsync();
+            }
+
+            private async Task CompleteAsync()
+            {
+                try
+                {
+                    //一部等待任何运行中的操作
+                }catch(Exception ex)
+                {
+                    _completion.TrySetException(ex);
+                }
+                finally
+                {
+                    _completion.TrySetResult(null);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            UseMyInterfaceAsync(new MyAsyncClass()).Wait();
+            var factory= MyAsyncClassFactory.createAsync().Result;
+            Console.WriteLine(factory);
+            var funType = new MyFundamentalType();
+            funType.Initialisation.Wait();
+            Console.WriteLine(funType);
+            var data = new AsyncProperty().Data.GetValue();
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ Learn_CSharp/concurrency_in_CSharp/CP11/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace CP11
+{
+    class Program
+    {
+        //异步数据没有保护的情况
+        class SharedData
+        {
+            public int Value { get; set; }
+        }
+        async Task ModifyValueAsync(SharedData data)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            data.Value = data.Value + 1;
+        }
+        //没有同步的部分
+        async Task<int> ModifyValueConcurrentlyAsync()
+        {
+            var data = new SharedData();
+            var task1 = ModifyValueAsync(data);
+            var task2 = ModifyValueAsync(data);
+            var task3 = ModifyValueAsync(data);
+            await Task.WhenAll(task1, task2, task3);
+            return data.Value;
+        }
+
+        //下面一个代码，简单的并行修改同一个数据
+        async Task<int> SimpleParalelismAsync()
+        {
+            int val = 0; //即使是既不变量，他可能也需要同步
+            var task1 = Task.Run(() => { val++; });
+            var task2 = Task.Run(() => { val++; });
+            var task3 = Task.Run(() => { val++; });
+            await Task.WhenAll(task1, task2, task3);
+            return val;
+        }
+
+        //坏代码
+        int ParallelSum(IEnumerable<int> values)
+        {
+            int result = 0;
+            //最后一步的累加，没有同步
+            Parallel.ForEach(source: values, localInit: () => 0, body: (item, state, localValue) => localValue + item, localFinally: localValue => { result += localValue; });
+            return result;
+        }
+
+        //使用不可变集合时需要同步的情况
+        async Task<bool> PlayWithStackAsync()
+        {
+            var stack = ImmutableStack<int>.Empty;
+
+            var task1 = Task.Run(() => { stack = stack.Push(3); });
+            var task2 = Task.Run(() => { stack = stack.Push(5); });
+            var task3 = Task.Run(() => { stack = stack.Push(7); });
+
+            await Task.WhenAll(task1, task2, task3);
+            return stack.IsEmpty;
+        }
+
+        //使用阻塞锁
+        class MyClassLock
+        {
+            //这个锁会保护_value
+            private readonly object _mutex = new object();
+            private int _value;
+            public void Increment()
+            {
+                lock(_mutex)
+                {
+                    _value++;
+                }
+            }
+        }
+        
+        //异步锁
+        class MyClassSemaphore
+        {
+            //这个锁保护_value
+            private readonly SemaphoreSlim _mutex = new SemaphoreSlim(1);
+            private int _value;
+            public int getValue()
+            {
+                return _value;
+            }
+
+            public async Task DelayAndIncrementAsync()
+            {
+                await _mutex.WaitAsync();
+                try
+                {
+                    var oldValue = _value;
+                    await Task.Delay(TimeSpan.FromSeconds(oldValue));
+                    _value = oldValue + 1;
+                }
+                finally
+                {
+                    _mutex.Release();
+                }
+            }
+        }
+
+        //使用阻塞信号
+        class MyClassManualResetEvent
+        {
+            private readonly ManualResetEventSlim _initialised = new ManualResetEventSlim();
+            private int _value;
+            
+            public int WaitForInitialisation()
+            {
+                _initialised.Wait();
+                return _value;
+            }
+
+            public void InitialiseFromAnotherThread()
+            {
+                _value = 13;
+                _initialised.Set();
+            }
+        }
+
+        //发送异步信号
+        //有点go语言channel的那个 味儿了
+        class MyClassSignal
+        {
+            private readonly TaskCompletionSource<object> _initialised = new TaskCompletionSource<object>();
+
+            private int _value1;
+            private int _value2;
+
+            public async Task<int> WaitForinitialsationAsync()
+            {
+                await _initialised.Task;
+                return _value1 + _value2;
+            }
+
+            public void Initialise()
+            {
+                _value1 = 13;
+                _value2 = 17;
+                _initialised.TrySetResult(null);
+            }
+        }
+
+        //下面的代码是限流的相关
+        //数据流的限流
+        IPropagatorBlock<int,int> DataflowMultiplyBy2()
+        {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = 10
+            };
+            return new TransformBlock<int,int>(data=>data*2, options);
+        }
+        //PLINQ的限流
+        IEnumerable<int> ParallelMultiplyBy2(IEnumerable<int> values)
+        {
+            return values.AsParallel().WithDegreeOfParallelism(10).Select(item => item * 2);
+        }
+
+        //使用Paralel类
+        void ParallelCalculate(IEnumerable<int> values)
+        {
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 10
+            };
+            Parallel.ForEach(values, options, data => data *= 2);
+        }
+
+        //并发性异步代码可以用SemaphoreSlim来限流
+        async Task<string[]> DownloadUrlsAsync(IEnumerable<string> urls)
+        {
+            var httpClient = new HttpClient();
+            var semaphore = new SemaphoreSlim(10); //最高设置为0
+            var tasks = urls.Select(async url =>
+              {
+                  await semaphore.WaitAsync();
+                  try
+                  {
+                      return await httpClient.GetStringAsync(url);
+                  }
+                  finally
+                  {
+                      semaphore.Release();
+                  }
+              }).ToArray();
+            return await Task.WhenAll(tasks);
+        }
+
+        static void Main(string[] args)
+        {
+            //本应该输出3，但实际可能不是
+            Console.WriteLine(new Program().ModifyValueConcurrentlyAsync().Result);
+            //本应该输出3，但实际可能不是
+            Console.WriteLine(new Program().SimpleParalelismAsync().Result);
+            var sepahoreClss = new MyClassSemaphore();
+            sepahoreClss.DelayAndIncrementAsync().Wait();
+            sepahoreClss.DelayAndIncrementAsync().Wait();
+            Console.WriteLine(sepahoreClss.getValue());
+            var classSignal = new MyClassSignal(); 
+            Console.WriteLine("class signal has been created");
+            Task.Run(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(2));
+                classSignal.Initialise();
+            });
+            Console.WriteLine(classSignal.WaitForinitialsationAsync().Result);
+        }
+    }
+}
