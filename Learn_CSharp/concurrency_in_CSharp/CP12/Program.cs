6167567e5b59ae056be6481ff15744431670e452
孙楷 <704725181@qq.com>
2019-12-05 19:13:48 +0800

    C#并发编程经典实例 第12，13章
    第12章 调度
    第13章 实用技巧

---
+++ Learn_CSharp/concurrency_in_CSharp/CP12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace CP12
+{
+    class Program
+    {
+        //使用调度器控制并行代码
+        void CalculateValues(IEnumerable<int> collections)
+        {
+            var schedulerPair = new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, maxConcurrencyLevel: 8);
+            TaskScheduler scheduler = schedulerPair.ConcurrentScheduler; //限制自身并发的对象，这时通常不需要使用ExclusiveScheduler
+            ParallelOptions options = new ParallelOptions { TaskScheduler = scheduler };
+            Parallel.ForEach(collections, options, data => data++);
+        }
+
+        //用调度器实现数据流的同步
+        void TaskDataFlow()
+        {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                TaskScheduler = TaskScheduler.FromCurrentSynchronizationContext()
+            };
+            var mutiplyBlock = new TransformBlock<int, int>(item => item * 2);
+            var displayBlock = new ActionBlock<int>(result => Console.WriteLine(result));
+            mutiplyBlock.LinkTo(displayBlock);
+
+        }
+        static void Main(string[] args)
+        {
+            //使用Task.Run而将任务调度到线程池
+            Task task = Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            });
+            //Task同样也可以用异步表达式
+            Task<int> task2 = Task.Run(async () =>
+              {
+                  await Task.Delay(TimeSpan.FromSeconds(1));
+                  return 13;
+              });
+        }
+    }
+}
---
+++ Learn_CSharp/concurrency_in_CSharp/CP13/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CP13
+{
+    class Program
+    {
+        //使用Lazy来进行初始化
+        static int _simpleValue;
+        static readonly Lazy<int> MySharedInteger = new Lazy<int>(() => _simpleValue++);
+        //如果初始化过程中需要执行异步任务，可以采用一个非常类似的方法
+        static readonly Lazy<Task<int>> MySharedAsyncInteger = new Lazy<Task<int>>(async () =>
+          {
+              await Task.Delay(TimeSpan.FromSeconds(2)).ConfigureAwait(false);
+              return _simpleValue++;
+          });
+        int UseSharedInteger()
+        {
+            //使用Lazy类
+            //不管有多少个线程调用这个类，工厂委托都只会运行一次。
+            int sharedValue = MySharedInteger.Value;
+            return sharedValue;
+        }
+        async Task<int> GetSharedIntegerAsync()
+        {
+            int sharedValue = await MySharedAsyncInteger.Value;
+            return sharedValue;
+        }
+
+
+        static async Task<int> GetValueAsync()
+        {
+            Console.WriteLine("Calling server...");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            Console.WriteLine("Return result...");
+            return 13;
+        }
+        static void Main(string[] args)
+        {
+            var program= new Program();
+            var task = program.GetSharedIntegerAsync();
+            int value = program.UseSharedInteger();
+            Console.WriteLine(value);
+            Console.WriteLine(task.Result);
+
+            //Rx延迟求值
+            var invokeServerObservable = Observable.Defer(() => GetValueAsync().ToObservable());
+            invokeServerObservable.Subscribe(_=>{ });
+            invokeServerObservable.Subscribe(_ => { });
+            Console.ReadKey();
+        }
+    }
+}
