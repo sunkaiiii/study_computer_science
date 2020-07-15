56ec863ce4541a2d56f2bee25627d3ff0a2e57a1
孙楷 <704725181@qq.com>
2020-07-15 19:55:44 +1000

    ASP.NET Core in Action Chapter 9
    Chapter 9 Creating a Web API for mobile and client applications using MVC

---
+++ Learn_CSharp/ASP.NET Core in Action/FirstApplication/FirstApplication/Controllers/FruitController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstApplication.Controllers
+{
+    public class FruitController : Controller
+    {
+        List<string> _fruit = new List<string>
+        {
+        "Pear",
+        "Lemon",
+        "Peach"
+        };
+        public IEnumerable<string> Index()
+        {
+            return _fruit;
+        }
+
+        public IActionResult View(int id)
+        {
+            if(id>=0 && id < _fruit.Count)
+            {
+                return Ok(_fruit[id]);
+            }
+            return NotFound();
+        }
+
+        [Route("{controller}/getFruit/{fruitId=0:int}")] //set default value
+        public IActionResult getFruit(int fruitId)
+        {
+            return View(fruitId);
+        }
+    }
+}
