e46f5ec43753ddf975c5b15cc0d357687d9e3a6e
孙楷 (Kai) <ksun0017@qq.com>
2020-12-30 23:08:19 +1100

    CLR_Via_C# chapter1
    Chapter 1 CLR的执行模型

---
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//强制符合CLS类型
+[assembly: CLSCompliant(true)]
+namespace Chapter1
+{
+    public sealed class Program
+    {
+        //警告，返回类型不符合CLS   
+        public UInt32 Abc() => 0;
+
+        //进大小写不同标识符，不符合CLS
+        public void abc() { }
+
+        private UInt32 ABC() => 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
