9991fe274cdee097bafa2680e6dfc7ff05117712
孙楷 (Kai) <ksun0017@qq.com>
2021-01-21 22:42:54 +1100

    C#并发编程经典实例第二版 chapter 13
    第十三章 调度

---
+++ Learn_CSharp/concurrency_in_CSharp_2th_edition/Chapter13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Chapter13
+{
+    class Program
+    {
+        // 调度并行代码
+        void CalculateInts(IEnumerable<int> collections)
+        {
+            var scuedulerPair = new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, maxConcurrencyLevel: 8);
+            TaskScheduler scheduler = scuedulerPair.ConcurrentScheduler;
+            ParallelOptions options = new ParallelOptions { TaskScheduler = scheduler };
+            Parallel.ForEach(collections, options, i => i++);
+        }
+
+        void ScheduleTPL()
+        {
+            var options = new ExecutionDataflowBlockOptions
+            {
+                TaskScheduler = TaskScheduler.FromCurrentSynchronizationContext()
+            };
+            var multiplyBlock = new TransformBlock<int, int>(item => item * 2);
+            var displayBlock = new ActionBlock<int>(result => Console.WriteLine(result));
+            multiplyBlock.LinkTo(displayBlock);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
