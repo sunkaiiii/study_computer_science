1f04666021af6750a959c20d89db182f72d3fe95
孙楷 (Kai) <ksun0017@qq.com>
2021-01-28 23:07:56 +1100

    CLR Via C# Chapter 9
    第九章 参数

---
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Chapter9
+{
+    class Program
+    {
+        //C# 允许对ref/out类型的重载
+        static void Add(String s)
+        {
+
+        }
+
+        static void Add(ref String s)
+        {
+
+        }
+
+        //使用泛型来实现类的交换
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T t = b;
+            b = a;
+            a = t;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
