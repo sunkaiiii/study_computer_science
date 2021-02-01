5abd147e6b6639f66a6e7294bb35db4ddfded7f6
孙楷 (Kai) <ksun0017@qq.com>
2021-02-01 16:15:26 +1100

    CLR Via c# chapter12
    第十二章 泛型

---
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter12
+{
+    //一个部分指定的开放类型
+    internal sealed class DictionaryStringKey<TValue> : Dictionary<String, TValue>
+    {
+
+    }
+
+    class Program
+    {
+        //当前CLR还不支持泛型类型变量作为操作数使用
+        //private static T Sum<T>(T num)where T : struct
+        //{
+        //    T sum = default(T);
+        //    for (T n = default(T); n < num; n++)
+        //        sum += n;
+        //    return sum;
+        //}
+
+        static void Main(string[] args)
+        {
+            object o = null;
+            //开放类型，有两个未指明的类型参数
+            Type t = typeof(Dictionary<,>);
+
+            o = CreateInstance(t);
+            Console.WriteLine();
+
+
+            //DictionaryStringKey是开放类型，有1个类型参数
+            t = typeof(DictionaryStringKey<>);
+
+            o = CreateInstance(t);
+            Console.WriteLine();
+
+            //DictionaryStringKey<Guid>是封闭类型
+            t = typeof(DictionaryStringKey<Guid>);
+            o = CreateInstance(t);
+            Console.WriteLine(" 对象类型 = " + o.GetType());
+        }
+
+        private static object CreateInstance(Type t)
+        {
+            object o = null;
+            try
+            {
+                o = Activator.CreateInstance(t);
+                Console.Write("已创建{0}的实例", t.ToString());
+            }catch(ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return o;
+        }
+    }
+}
--- Learn_Python/Python基础教程（第三版）/chapter16/performance.py
+++ Learn_Python/Python基础教程（第三版）/chapter16/performance.py
@@ -1,3 +1,3 @@
-import cProfile
+import cProfile # 性能分析
 from doctest_example import product
 cProfile.run('product(1,2)')
