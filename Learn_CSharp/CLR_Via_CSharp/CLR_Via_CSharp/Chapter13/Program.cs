eee722ed2f0a9a0025cb2fc8459bddf31909f2bc
孙楷 (Kai) <ksun0017@qq.com>
2021-02-05 00:09:54 +1100

    CLR Via C# Chapter 14
    第十四章 字符、字符串和文本处理

--- Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter13/Program.cs
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Chapter13
 {
@@ -17,6 +18,29 @@ namespace Chapter13
                 Console.WriteLine("IDisposable Dispose");
             }
         }
+
+        //可以实现泛型接口两次
+        public sealed class Number : IComparable<Int32>, IComparable<string>
+        {
+            private int m_val = 5;
+            public int CompareTo([AllowNull] int other)
+            {
+                return m_val.CompareTo(other);
+            }
+
+            public int CompareTo([AllowNull] string other)
+            {
+                return m_val.CompareTo(int.Parse(other));
+            }
+
+            //泛型接口的约束
+            private static int M<T>(T t)where T: IComparable, IConvertible
+            {
+                return 0;
+            }
+        }
+
+
         static void Main(string[] args)
         {
             SimpleType st = new SimpleType();
---
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter14/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace Chapter14
+{
+    class Program
+    {
+        internal sealed class BoldInt32s : IFormatProvider, ICustomFormatter
+        {
+            //使用定制格式化器来实现将int32值标记<B></B>
+            public Object GetFormat(Type formartType)
+            {
+                if (formartType == typeof(ICustomFormatter))
+                    return this;
+                return Thread.CurrentThread.CurrentCulture.GetFormat(formartType);
+            }
+
+            public string Format(string format, object arg,IFormatProvider formatProvider)
+            {
+                string s;
+                IFormattable formattable = arg as IFormattable;
+                if (formattable == null)
+                {
+                    s = arg.ToString();
+                }
+                else
+                {
+                    s = formattable.ToString(format, formatProvider);
+                }
+                if(arg.GetType()==typeof(Int32))
+                {
+                    return "<B>" + s + "</B>";
+                }
+                return s;
+            }
+        }
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat(new BoldInt32s(), "{0} {1} {2:M}", "Kai", 123, DateTime.Now);
+            Console.WriteLine(sb.ToString());
+
+            //使用UTF8进行编码
+            string s = "hi there";
+            Encoding encodingUTF8 = Encoding.UTF8;
+            byte[] encodedBytes = encodingUTF8.GetBytes(s);
+            Console.WriteLine("Encoded bytes: " + BitConverter.ToString(encodedBytes));
+            string decodedString = encodingUTF8.GetString(encodedBytes);
+            Console.WriteLine("Decoded sting: " + decodedString);
+        }
+    }
+}
