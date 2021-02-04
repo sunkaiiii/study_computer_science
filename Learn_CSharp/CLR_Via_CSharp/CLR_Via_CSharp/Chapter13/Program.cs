e2219336eeb71935f7bfd3c867d014c82328a0f3
孙楷 (Kai) <ksun0017@qq.com>
2021-02-04 12:10:20 +1100

    CLR Via C# Chapter 13
    第十三章 接口

---
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chapter13
+{
+    class Program
+    {
+        sealed class SimpleType:IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("public dispose");
+            }
+
+             //创建显示接口方法实现
+             void IDisposable.Dispose()
+            {
+                Console.WriteLine("IDisposable Dispose");
+            }
+        }
+        static void Main(string[] args)
+        {
+            SimpleType st = new SimpleType();
+            st.Dispose();
+            IDisposable d = st;
+            d.Dispose();
+        }
+    }
+}
