cd7e026c3abec70544b660003cacf9a6bd068a6a
孙楷 (Kai) <ksun0017@qq.com>
2021-02-06 00:52:07 +1100

    CLR Via C# Chapter 18
    第十八章 定制特性

---
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chapter18
+{
+    //定制属性
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Enum, Inherited = false)]
+    public class FlagsAttribute : System.Attribute
+    {
+
+        public FlagsAttribute()
+        {
+
+        }
+    }
+    class Program
+    {
+        [Flags]
+        enum Test
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
