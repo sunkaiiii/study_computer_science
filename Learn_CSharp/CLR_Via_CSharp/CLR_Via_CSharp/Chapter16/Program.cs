f615fc97df9ef8341d736ed1c54bf0747dc41e01
孙楷 (Kai) <ksun0017@qq.com>
2021-02-05 17:21:34 +1100

    CLR Via C# Chapter 16
    第十六章 数组

---
+++ Learn_CSharp/CLR_Via_CSharp/CLR_Via_CSharp/Chapter16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Chapter16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //数组协变性引发的后果
+            string[] sa = new string[100];
+            object[] oa = sa; //协变性
+            oa[5] = "Kai"; //性能损失，CLR要对类型进行检查
+                           // oa[3] = 3; //抛出异常
+
+            //栈上数组
+            StackAllocDemo();
+        }
+
+        private static void StackAllocDemo()
+        {
+            unsafe
+            {
+                const int width = 20;
+                char* pc = stackalloc char[width]; //在栈上分配数组
+                string s = "Kai Sun";
+                for(int index = 0;index<width;index++)
+                {
+                    pc[width - index - 1] = (index < s.Length) ? s[index] : '.';
+                }
+                Console.WriteLine(new string(pc, 0, width));
+            }
+          
+        }
+    }
+}
