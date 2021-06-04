d9575bb7e0135dcafc19eb634e70b45159849080
Kai Sun <ksun0017@qq.com>
2021-06-04 19:59:30 +1000

    C# in depth Chapter 2

---
+++ Learn_CSharp/C#_in_depth (4th edition)/CS_in_depth/CS_in_depth/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS_in_depth
+{
+    
+    class Program
+    {
+        //生成整形迭代器
+        static IEnumerable<int> CreateSimpleIterator()
+        {
+            yield return 10;
+            for (int i = 0; i < 3; i++)
+            {
+                yield return i; 
+            }
+            yield return 20;
+        }
+
+        //延迟执行的重要性
+        static IEnumerable<int> Fibonacci()
+        {
+            int current = 0;
+            int next = 1;
+            while (true)
+            {
+                yield return current;
+                int oldCurrent = current;
+                current = next;
+                next = next + oldCurrent;
+            }
+        }
+
+        static void PrintType<T>()
+        {
+            Console.WriteLine("typeof(T) = {0}", typeof(T));
+            Console.WriteLine("typeof(List<T>) = {0}", typeof(List<T>));
+        }
+
+        static void Main(string[] args)
+        {
+            PrintType<string>(); 
+            PrintType<int>();
+
+            foreach (int i in CreateSimpleIterator())
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Print fibonacci");
+            foreach(var value in Fibonacci())
+            {
+                Console.WriteLine(value);
+                if (value > 1000)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
