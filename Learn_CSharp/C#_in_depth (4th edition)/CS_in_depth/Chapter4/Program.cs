fc46815395f399d1bf641f728ad60f750b5d633c
孙楷 (Kai Sun) <ksun0017@qq.com>
2021-06-11 21:41:04 +1000

    C# in depth Chapter 3
    Chapter 4: C# 4

---
+++ Learn_CSharp/C#_in_depth (4th edition)/CS_in_depth/Chapter4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Chapter4
+{
+    class Program
+    {
+        static void Add(dynamic d)
+        {
+            Console.WriteLine(d + d);
+        }
+        static void Main(string[] args)
+        {
+            //使用不同的值调用方法
+            Add("Text");
+            Add(10);
+            Add(TimeSpan.FromMinutes(45));
+        }
+    }
+}
