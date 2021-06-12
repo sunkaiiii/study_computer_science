28efac9e801034b5e460077e5ff71265e95d30ea
孙楷 (Kai Sun) <ksun0017@qq.com>
2021-06-12 23:02:12 +1000

    C# in depth Chapter 11
    Chapter 11 Tuple

---
+++ Learn_CSharp/C#_in_depth (4th edition)/CS_in_depth/Chapter11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter11
+{
+    class Program
+    {
+        //元组的使用
+        static (int min, int max) MinMax(IEnumerable<int> source)
+        {
+            int max = source.Max();
+            int min = source.Min();
+            return (min, max);
+        }
+        static void Main(string[] args)
+        {
+            int[] values = { 2, 7, 3, -5, 1, 0, 10 };
+            var extremes = MinMax(values);
+            Console.WriteLine(extremes.min);
+            Console.WriteLine(extremes.max);
+        }
+    }
+}
